08c91ef1fb924c14afa7f49d3071d1480ad052ae
kishor <kishorrathva8298@gmail.com>
2018-11-28 03:23:23 +0530

    SelectionSort

---
+++ SelectionSort/SelectionSort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int temp, min, n, a[20];
+    printf("How many Number Do you want to Sort...?(Max-20)\n");
+    scanf("%d", &n);
+    printf("Enter %d Numbers:..\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("Before Sorting >>>\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("[ %d ]", a[i]);
+    }
+    printf("\n");
+    //Logic
+    for (int i = 0; i < n; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[min])
+                min = j;
+        }
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+
+    printf("After Sorting >>>\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("[ %d ]", a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
